2ef42aed73581b3e5e635c4bf883c95d2a06af23
Michał Cukierman <michal.cukierman@ds.pl>
2022-09-22 14:08:44 +0200

    [WS-2918] Improved startup script

--- docs/scripts/get.sh
+++ docs/scripts/get.sh
@@ -1,7 +1,9 @@
 if [ -x "$(command -v docker)" ]; then
+    rm -rf websight-cms-ce/
     mkdir websight-cms-ce
     cd websight-cms-ce
-    curl -s https://www.websight.io/scripts/docker-compose.yml --output docker-compose.yml
+    curl -s localhost:8000/scripts/docker-compose.yml --output docker-compose.yml
+    (sleep 10 && open http://localhost:8080)&
     docker compose up
 else
     echo "Docker is not found on the system"
